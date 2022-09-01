e6fd60b84a0cc3592848cc990a223378c356c3bb
luan <luan@contentsys.com.br>
2022-09-01 08:53:47 -0300

    Cria o registro na inicializacao do aplicativo

--- nodejs/index.js
+++ nodejs/index.js
@@ -10,18 +10,37 @@ const port = "8000";
 const sqlInsert = `insert into people(name) values(?)`;
 const sqlSelect = `select * from people`;
 
-app.get("/", async (req, res) => { 
-        
-    const connection = await mysql.createConnection({ 
+
+const createMysqlConnection = async ()=> {
+    const connection = mysql.createConnection({ 
         host: 'db',
         user: 'root',
         password: 'root',
         database: 'nodedb'
     });
+
+    return connection;
+}
+const criaRegistroNoBanco = async () => {
+
+    const connection = await createMysqlConnection();
+
+    sqlCreateTable = `CREATE TABLE IF NOT EXISTS people(id int not null auto_increment, name varchar(255), primary key (id))`;
+    await connection.query(sqlCreateTable);
+
     const chance = new Chance();
     const name = chance.name({nationality: 'it'});
     
     await connection.query(sqlInsert, [name] );
+
+    connection.end();
+
+}
+
+app.get("/", async (req, res) => { 
+
+
+    const connection = await createMysqlConnection();
 
     const [people] = await connection.query(sqlSelect);
 
@@ -38,6 +57,8 @@ app.get("/", async (req, res) => {
     ${peopleNames}
     `)
 });
+
+criaRegistroNoBanco();
 
 app.listen(port, ()=> {
     console.log(`Express is Running on ${port}`);
